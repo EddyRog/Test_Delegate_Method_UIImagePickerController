f4bc4558f96ae101ab098caba71c2c2029ff9371
EddyRog <pro.eddy.r@gmail.com>
2021-07-17 17:52:31 +0200

    Defaut init

---
+++ App03Tests/ViewControllerTests.swift
@@ -0,0 +1,14 @@
+//
+//  ViewControllerTests.swift
+//  App03Tests
+//
+//  Created by Eddy R on 17/07/2021.
+//
+
+import XCTest
+
+class ViewControllerTests: XCTestCase {
+    func testTrue() {
+        XCTAssertTrue(true)
+    }
+}
