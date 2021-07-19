7566f4e2b576de3650cacb09c997daa71212a84b
EddyRog <pro.eddy.r@gmail.com>
2021-07-19 10:12:11 +0200

    update

--- App03Tests/ViewControllerTests.swift
+++ App03Tests/ViewControllerTests.swift
@@ -9,6 +9,23 @@ import XCTest
 @testable import App03
 
 class ViewControllerTests: XCTestCase, UIImagePickerControllerDelegate {
+    var sut: ViewController!
+
+    override func setUp() {
+        super.setUp()
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        if let vc = storyboard.instantiateInitialViewController() as? ViewController {
+            _ = vc.view
+            sut = vc
+        }
+
+
+    }
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+
     func testTrue() {
         XCTAssertTrue(true)
     }
